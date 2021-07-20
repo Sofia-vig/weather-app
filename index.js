fb0720114247fdec5be4d48ef2960f22a2cdfe40
sofia <sofiavign@gmail.com>
2021-07-20 21:50:47 +0200

    comentarios

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-//Div principal contenido clima
+//Main div
 function divWeatherInfo(json) {
   //Title
   const titleEl = document.querySelector(".weather-title-city");
   titleEl.textContent = json.city.name;
 
-  //Imagen
+  //Img
   const logoEl = document.querySelector(".logo-weather");
   const cloud = json.list[0].clouds.all;
   if (cloud < 50) {
@@ -13,21 +13,22 @@ function divWeatherInfo(json) {
     logoEl.src = "src/cloud1.png";
   }
 
-  //Grados
+  //Degrees
   const degreeEl = document.querySelector(".wather-degrees");
   const round = Math.round(json.list[0].main.temp);
   degreeEl.textContent = round + "°C";
 }
 
-//Divs abajo items
+//Divs
 function itemWeather(json, item) {
   const template = document.querySelector(".template-day-after");
   const container = document.querySelector(".content-days-after");
-  //Grados
+
+  //Degrees
   const round = Math.round(json[item].main.temp);
   template.content.querySelector(".day-degrees").textContent = round + "°C";
 
-  //Imagen
+  //Img
   const clouds = json[item].clouds.all;
   if (clouds < 50) {
     template.content.querySelector(".logo-day").src = "src/sun1.png";
@@ -35,13 +36,13 @@ function itemWeather(json, item) {
     template.content.querySelector(".logo-day").src = "src/cloud1.png";
   }
 
-  //Fecha
+  //Date
   const arrayCortado = json[item].dt_txt.split(" ")[0];
   const resuArray = arrayCortado.split("-").reverse();
   const day = resuArray[0] + "/" + resuArray[1];
   template.content.querySelector(".day-date").textContent = day;
 
-  //Clonar
+  //Clone
   var clone = document.importNode(template.content, true);
   container.appendChild(clone);
 }
